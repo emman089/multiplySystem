19fa59e0cb05706ceb807e75a054df8d51dc5e3f
emman089 <emmanuellabuson04@gmail.com>
2025-01-25 03:44:46 +0800

    Update auth-route.js

--- backend/routes/auth-route.js
+++ backend/routes/auth-route.js
@@ -1,16 +1,6 @@
 import express from 'express';
-import { signup,login,logout, verifyEmail, resetPassword, forgotPassword, checkAuth, checkMember, checkMemberTransaction  } from '../controllers/auth-controller.js';
-import { verifyToken } from '../middleware/verifyToken.js';
+import {login  } from '../controllers/auth-controller.js';
 const router = express.Router();
 
-router.post('/signup', signup)
 router.post('/login', login)
-router.post('/logout', logout)
-router.post('/verify-email', verifyEmail)
-router.post('/forgot-password', forgotPassword)
-router.post('/reset-password/:token', resetPassword)
-router.get('/check-auth', verifyToken, checkAuth)
-router.get('/check-member', checkMember)
-router.get('/check-transaction', verifyToken, checkMemberTransaction)
-
 export default router;
