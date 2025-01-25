de2d2ce3ca4e1c6c7ee482a0044e30e01a2df66b
emman089 <emmanuellabuson04@gmail.com>
2025-01-25 10:00:04 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://multiply-system-qerccrazk-emman089s-projects.vercel.app", // Your frontend URL (adjust if different)
+    origin: "multiply-system-yxiqj4th3-emman089s-projects.vercel.app", // Your frontend URL (adjust if different)
     credentials: true,  // Allow cookies
 }));
 
